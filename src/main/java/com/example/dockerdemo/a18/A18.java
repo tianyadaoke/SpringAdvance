fd6064c91aa7f7656fa6901271498732f489819b
Bing Zhou <bingfbi@gmail.com>
2022-04-07 16:52:12 +0200

    高级切面转低级切面

---
+++ src/main/java/com/example/dockerdemo/a18/A18.java
@@ -0,0 +1,70 @@
+package com.example.dockerdemo.a18;
+
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.aop.Advisor;
+import org.springframework.aop.aspectj.AspectInstanceFactory;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+import org.springframework.aop.aspectj.AspectJMethodBeforeAdvice;
+import org.springframework.aop.aspectj.SingletonAspectInstanceFactory;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+public class A18 {
+    public static void main(String[] args) {
+        // 高级切面转低级切面
+        List<Advisor> advisors=new ArrayList<>();
+        for (Method method : Aspect.class.getDeclaredMethods()) {
+            AspectInstanceFactory factory = new SingletonAspectInstanceFactory(new Aspect());
+            if (method.isAnnotationPresent(Before.class)) {
+                // 解析切点
+                String expression = method.getAnnotation(Before.class).value();
+                AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+                pointcut.setExpression(expression);
+                // 通知类
+                AspectJMethodBeforeAdvice beforeAdvice = new AspectJMethodBeforeAdvice(method, pointcut, factory);
+                // 切面
+                DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(pointcut, beforeAdvice);
+                advisors.add(advisor);
+            }
+        }
+        for (Advisor advisor : advisors) {
+            System.out.println(advisor);
+        }
+    }
+
+    static class Aspect {
+        @Before("execution(* foo())")
+        public void before1() {
+            System.out.println("before1");
+        }
+
+        @Before("execution(* foo())")
+        public void before2() {
+            System.out.println("before2");
+        }
+
+        public void after() {
+            System.out.println("after");
+        }
+
+        public void afterReturning() {
+            System.out.println("after returning");
+        }
+
+        public Object around(ProceedingJoinPoint pjp) throws Throwable {
+            return pjp.proceed();
+        }
+
+    }
+
+    static class Target {
+        public void foo() {
+            System.out.println("foo");
+        }
+    }
+}
